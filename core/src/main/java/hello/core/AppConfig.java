da7679b291db190131447df5418b2db11ec3b6d6
GeomJa <52377043+donut0310@users.noreply.github.com>
2022-07-22 13:50:03 +0900

    스프링으로 전환
    - AppConfig 파일에 어노테이션 추가
    @configuration, @Bean
    
    - 메인 메소드 변경
    import ApplicationContext, AnnotationConfigApllicationContext

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,12 +2,15 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성"
@@ -15,20 +18,27 @@ import hello.core.order.OrderServiceImpl;
  * "DIP 완성"
  * "관심사의 분리" => 객체를 생성하고 연결하는 역할과, 실행하는 역할이 명확히 분리됨
  */
+
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 할인 정책 변경, 사용 영역의 코드는 변경할 필요 x, 구성 영역의 코드만 변경
     }
 }
