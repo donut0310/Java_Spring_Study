77d379e7aa109a1d10c1c9015fd11b23e1c1d21c
GeomJa <52377043+donut0310@users.noreply.github.com>
2022-08-01 11:25:44 +0900

    의존 관계 자동 주입
    - 자동, 수동 주입의 올바른 실무 운영 기준

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member", // 탐색 시작 위치 지정
+        basePackages = "hello.core", // 탐색 시작 위치 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // => @Component 어노테이션이 붙은 클래스를 찾아 자동으로 스프링 빈으로 등록
 public class AutoAppConfig {
