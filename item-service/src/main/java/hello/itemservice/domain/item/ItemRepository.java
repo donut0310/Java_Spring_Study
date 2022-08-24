ab867116e047826d4fd411f0d68b10f95d67eb26
GeomJa <52377043+donut0310@users.noreply.github.com>
2022-08-24 12:06:58 +0900

    스프링 MVC 실제 응용
    상품 관리 서비스 만들기
    - 상품 도메인
    - 상품 목록
    - 상품 상세
    - 상품 등록 폼

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,42 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); //static
+    // 멀티 스레드 환경에서 여러개가 동시에 접근하기 때문에 HashMap을 사용하면 안된다 (실무에선!) 대신 ConcurrentHashMap을 사용해야 함
+    private static long sequence = 0L; //static
+    // 위와 마찬가지로 long 대신 automic을 사용해야함
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
